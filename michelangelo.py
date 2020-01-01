cc690fce719ba0385589dfdffbdb191f2b5b0cb8
TrueBinary <gui15787@gmail.com>
2020-01-01 01:08:37 -0300

    forked

---
+++ michelangelo.py
@@ -0,0 +1,166 @@
+#lib importantes
+#-*- coding: utf-8 -*-
+
+#Copyright (C) 2008 MrTrue <gui19787@gmail.com>
+#     This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>
+
+
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+from time import sleep
+import configparser
+import logging
+import praw
+import os
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, DelayQueue
+from telegram.ext.dispatcher import run_async
+from google_images_download import google_images_download
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+					level=logging.DEBUG)
+
+
+logger = logging.getLogger(__name__)
+
+"""
+MODE = os.getenv("MODE")
+TOKEN = os.getenv("TOKEN")
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+USER_AGENT = os.getenv("USER_AGENT")
+"""
+config = configparser.ConfigParser()
+config.read("config.ini")
+MODE = config.get("bot_configs","mode")
+TOKEN = config.get("bot_configs","token")
+CLIENT_ID = config.get("bot_configs","client_id")
+CLIENT_SECRET = config.get("bot_configs","client_secret")
+USER_AGENT = config.get("bot_configs","user_agent")
+
+if MODE == "dev":
+	def run(updater):
+		updater.start_polling()
+		updater.idle()
+"""
+elif MODE == "proud":
+	def run(updater):
+		PORT = int(os.environ.get("PORT", "8843"))
+		HEROKU_APP_NAME = os.environ.get("tele-py")
+
+		updater.start_webhook(listen="0.0.0.0",
+							  port=PORT,
+							  url_path=TOKEN)
+		updater.bot.set_webhook(f"https://{HEROKU_APP_NAME}.herokuapp.com/{TOKEN}")
+
+else:
+	logger.error("No MODE specfied")
+	sys.exit(1)
+"""
+def start(bot,update):
+	chat_id = update.message.chat_id
+	msg_id = update.message.message_id
+	botwelcome = """Welcome to the pybobot i'll send to you some random images which you want,why do you not try send /get some shit ?"""
+	bot.send_message(chat_id=chat_id, text=botwelcome, reply_text=msg_id)
+
+def ajuda(bot,update):
+	update.message.reply_text("Use /get to get some random images of google")
+
+
+def info(bot,update):
+	info_text = """
+<b>Hi i'm bot of MrTrue,i was created to help you find some imagens on the google</b>
+<b>Developer: </b> <a href="https://t.me/Mr_True">MrTrue</a>
+<b>Code: </b> <a href="https://github.com/TrueBinary/TelePy">Github</a>
+	"""
+
+	bot.send_message(parse_mode="HTML",chat_id=update.message.chat_id, text=info_text, reply_text=update.message.chat)
+
+
+@run_async
+def get(bot,update,args,job_queue):
+	print(os.getcwd())
+	chat_id = update.message.chat_id
+	try:
+		if len(args) == 1:
+			keyword = args[0]
+			response= google_images_download.googleimagesdownload()
+			arguments = {"keywords":keyword,"limit":1,"no_directory":True}
+			paths = response.download(arguments)
+			bot.send_message(chat_id, text="wait for some seconds")
+			sleep(0.5)
+			for i in os.listdir("/home/MrTrue/telepy/downloads"):
+				nome = str(i)
+				dic = os.getcwd() + "/downloads/" + nome
+				if nome.endswith("gif"):
+					bot.sendDocument(chat_id=chat_id, document=open(dic,"rb"))
+				else:
+					bot.send_photo(chat_id, photo=open(dic,"rb"))
+				os.remove(f"{dic}")
+
+		elif len(args) == 2:
+			keyword = args[0]
+			sufkey = args[1]
+			response= google_images_download.googleimagesdownload()
+			arguments = {"keywords":keyword,"suffix_keywords":sufkey,"limit":1,"no_directory":True}
+			paths = response.download(arguments)
+			bot.send_message(chat_id, text= "wait for some seconds")
+			sleep(0.5)
+			for i in os.listdir("/home/MrTrue/telepy/downloads"):
+				nome = str(i)
+				dic = os.getcwd() + "/downloads/" + nome
+				if nome.endswith("gif"):
+					bot.sendDocument(chat_id=chat_id, document=open(dic,"rb"))
+				else:
+					bot.send_photo(chat_id, photo=open(dic,"rb"))
+				os.remove(f"{dic}")
+
+		else:
+			prekey = args[0]
+			keyword = args[1]
+			sufkey = args[2]
+			response= google_images_download.googleimagesdownload()
+			arguments = {"keywords":keyword,"suffix_keywords":sufkey,"prefix_keywords":prekey,"limit":1,"no_directory":True}
+			paths = response.download(arguments)
+			bot.send_message(chat_id, text= "wait for some seconds")
+			sleep(0.5)
+			for i in os.listdir("/home/MrTrue/telepy/downloads"):
+				nome = str(i)
+				dic = os.getcwd() + "/downloads/" + nome
+				if nome.endswith("gif"):
+					bot.sendDocument(chat_id=chat_id, document=open(dic,"rb"))
+
+				else:
+					bot.send_photo(chat_id, photo=open(dic,"rb"))
+				os.remove(f"{dic}")
+
+	except IndexError as e:
+		bot.send_message(chat_id=chat_id,text="Error none arguments")
+		print(f"some error we have here dev look at here {e}")
+
+
+def main():
+	updater = Updater(TOKEN)
+	dp = updater.dispatcher
+	dp.add_handler(CommandHandler("start", start))
+	dp.add_handler(CommandHandler("help",  ajuda))
+	dp.add_handler(CommandHandler("info", info))
+	dp.add_handler(CommandHandler("getimage", get, pass_args=True,))
+	dp.add_handler(CommandHandler("steam",send_reddit))
+	
+	updater.start_polling()
+	run(updater)
+
+if __name__== "__main__":
+	main()
+"""/TODO add new feature google reverse image """
